e9e17ab5afc9eff9fed6b6f3870510f5001ba195
farodey <farodey@yandex.ru>
2020-01-20 23:47:07 +0300

    add read to file

--- server.py
+++ server.py
@@ -4,12 +4,13 @@ import asyncore
 
 class CustomSMTPServer(smtpd.SMTPServer):
     def process_message(self, peer, mailfrom, rcpttos, data, **kwargs):
-        print('Receiving message from:', peer)
-        print('Message addressed from:', mailfrom)
-        print('Message addressed to  :', rcpttos)
-        print('Message length        :', len(data))
-        print(data.decode())
-        return
+        with open('db.txt', 'a') as file:
+            file.write('Receiving message from:' + str(peer) + '\n')
+            file.write('Message addressed from:' + str(mailfrom) + '\n')
+            file.write('Message addressed to  :' + str(rcpttos) + '\n')
+            file.write('Message length        :' + str(len(data)) + '\n')
+            file.write(data.decode() + '\n')
+            return
 
 
 server = CustomSMTPServer(('127.0.0.1', 1025), None)
