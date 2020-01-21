71e20d4ef5909954b1c86af3f99071307e949670
farodey <farodey@yandex.ru>
2020-01-21 23:05:20 +0300

    login/password

--- server.py
+++ server.py
@@ -3,23 +3,24 @@ import asyncore, re, base64
 
 
 class CustomSMTPServer(smtpd.SMTPServer):
-    count = 0
-
     def process_message(self, peer, mailfrom, rcpttos, data, **kwargs):
 
-        login = re.search(r"Логин: (.*?@hostname.com)", data)
-        password = re.search(r"Пароль: (\w{6})", data)
-
-        # Декодирует русские символы из письма
+        # Декодируем тело письма
         a = data.find('Subject')
         b = data.find('\n\n', a)
         body = data[b + 2: len(data)]
-        print(base64.b64decode(body).decode())
+        body = base64.b64decode(body).decode()
 
+        # Ищем логин/пароль
+        login = re.search(r"Логин: (.*?@yopmail.com)", body).group(1)
+        password = re.search(r"Пароль: (\w{6})", body).group(1)
+
+        # Пишем логин/пароль в файл
         with open('db.txt', 'a') as file:
-            file.write(" ".join([str(self.count)]))
-
-        self.count += 1
+            file.write(str(login))
+            file.write(':')
+            file.write(str(password))
+            file.write('\n')
 
 
 # Запускаем сервер
